d231ffa48e24066d25dedbf824f844545cef15bf
ilangnagomi <ilan@nagomisecurity.com>
2026-01-07 15:57:32 +0200

    comment added

---
+++ infrastructure/terraform-tutorial/lab_2/1_resource.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "local" {}
+
+# validation {
+#   condition     = can(regex("^[^ ]*$", var.file_name))
+#   error_message = "Spaces are not allowed in the file name."
+# }
+
+# Variable
+variable "file_name" {
+  type        = string
+  description = "Name of the student"
+  default     = "new_file"
+  validation {
+    condition     = can(regex("^[^ ]*$", var.file_name))
+    error_message = "Spaces are not allowed in the file name."
+  }
+}
+
+#comment
